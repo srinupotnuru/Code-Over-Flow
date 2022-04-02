edab0758e7905a26214a954fc38350b9728f3cd1
sriharsha <sriharsha@inncircles.com>
2022-04-03 02:09:06 +0530

    add sign in component

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
   styleUrls: ['./sign.component.scss']
 })
-export class SignComponent implements OnInit {
+
+
+export class SignComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('tab') tab!:ElementRef;
+
 
   constructor() { }
+
+  
+  ngAfterViewInit(): void {
+    console.log("The element i got is", this.tab);
+  }
 
   ngOnInit(): void {
     console.log("I got called inside SignComponent")
   }
+
+  
 
 }
--- src/app/sign/signin/signin.component.ts
+++ src/app/sign/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from 'src/services/login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signInForm!:FormGroup;
+
+  constructor(private router: Router, private route:ActivatedRoute, private fb: FormBuilder, private loginService: LoginService, private cookieService: CookieService) { }
 
   ngOnInit(): void {
+    this.setupForm();
+  }
+
+  setupForm(){
+    this.signInForm = this.fb.group({
+      regdnumber:['',[Validators.required]],
+      password:['',[Validators.required]],
+    })
+  }
+
+  get regdnumber() {
+    return this.signInForm.get('regdnumber');
+  }
+
+  get password(){
+    return this.signInForm.get('password');
+  }
+
+  onSubmit(){
+    console.log("the data is ", this.signInForm.value)
+    this.loginService.loginUser({'regdNumber':this.regdnumber?.value, 'password':this.password?.value}).subscribe((data:any)=>{
+        console.log("The response i got form backend is", data);
+        // cookie is set.
+        this.cookieService.set('jwt',data.data)
+    })
   }
 
 }
